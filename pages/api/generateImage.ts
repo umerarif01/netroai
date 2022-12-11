5a4625ac4aec561d26c5d86bf9616fb236332451
unknown <umerarif01@gmail.com>
2022-12-11 13:34:37 +0500

    first commit

---
+++ pages/api/generateImage.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+type Data = {
+  prompt?: string;
+  success?: boolean;
+  data?: string;
+  error?: unknown;
+};
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { prompt, size } = req.body;
+
+  const imageSize =
+    size === "Small" ? "256x256" : size === "Medium" ? "512x512" : "1024x1024";
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: imageSize,
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: "The image could not be generated",
+    });
+  }
+}
